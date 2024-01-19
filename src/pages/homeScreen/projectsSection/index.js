60b7788c1862c71eeaf7be402a5b0e5a0706b777
ved-blacksof <ved.blacksof@gmail.com>
2024-01-19 18:06:47 +0530

    added projects

--- src/pages/homeScreen/projectsSection/index.js
+++ src/pages/homeScreen/projectsSection/index.js
@@ -7,6 +7,13 @@ import "./style.css";
 export default function Projects() {
   const projectCards = [
     {
+      title: "Innogent",
+      des1: "Innogent  is a versatile software development company, specializing in React JS, Cloud Services, Java Development, AWS Cloud Computing, DevOps, and Big Data technologies.",
+      des2: "",
+      img2: placeholder,
+      href: "https://innogent.in/",
+    },
+    {
       title: "Varaha",
       des1: "Varaha offers carbon credits that are associated with specific projects related to sustainable agriculture and regenerative farming practices. ",
       des2: "",
@@ -35,6 +42,13 @@ export default function Projects() {
       href: "https://www.gavyamgentec.com/",
     },
     {
+      title: "The Sleek Studio",
+      des1: "Sleek Studio is a company that offers handcrafted luxury fashion for contemporary shoppers. They also curate personalized occasion-wear.",
+      des2: "",
+      img2: placeholder,
+      href: "https://thesleekstudio.com/",
+    },
+    {
       title: "System Level Solutions",
       des1: "system level solutions are designed to address complex problems and challenges by taking a comprehensive, integrated approach that considers all of the components and subsystems that make up a larger system.",
       des2: "",
@@ -43,7 +57,7 @@ export default function Projects() {
     },
     {
       title: "Blacksof",
-      des1: "Blacksof amplify the Brand Potential of Businesses with authenticity and shared values.      ",
+      des1: "Blacksof amplify the Brand Potential of Businesses with authenticity and shared values.",
       des2: "",
       img2: placeholder,
       href: "https://blacksof.com/",
@@ -51,19 +65,19 @@ export default function Projects() {
 
     {
       title: "Fasal",
-      des1: "Fasal is adklfj sdflksjfslk sddfjlkj.",
+      des1: "Fasal, a leader in precision farming, unveils FASAL WATER CREDIT™ to promote water-saving and cost-effective practices among farmers. Using an IoT-based AI platform, Fasal provides real-time, farm-specific recommendations based on on-farm sensor data, revolutionizing precision farming in horticulture.",
       des2: "",
       img2: placeholder,
       href: "https://fasal-website.vercel.app/",
     },
 
-    {
-      title: "Blogs_pot",
-      des1: "Blogs_pot description will come here.",
-      des2: "",
-      img2: blogspot,
-      href: "https://vedprakash25.github.io/blogs_pot/",
-    },
+    // {
+    //   title: "Blogs_pot",
+    //   des1: "Blogs_pot description will come here.",
+    //   des2: "",
+    //   img2: blogspot,
+    //   href: "https://vedprakash25.github.io/blogs_pot/",
+    // },
   ];
 
   return (
